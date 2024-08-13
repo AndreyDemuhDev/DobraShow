88d931399824715c4bdae58121fec12e5a12f202
Andrey Agnesh <AndreyAgnesh@yandex.by>
2024-08-13 23:43:16 +0300

    Refactor structure code (bottom navigation bar)

--- app/src/main/java/com/example/dobrashow/screens/show/ShowScreen.kt
+++ app/src/main/java/com/example/dobrashow/screens/show/ShowScreen.kt
@@ -18,16 +18,19 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.dobrashow.screens.show_details.LoadingStateContent
 import com.example.dobrashow.ui.components.CustomTopBarComponent
-import com.example.dobrashow.ui.components.SearchFieldComponent
 import com.example.dobrashow.ui.components.ShowItemCard
 import com.example.dobrashow.ui.theme.AppTheme
+import dev.chrisbanes.haze.HazeDefaults
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.haze
+import dev.chrisbanes.haze.hazeChild
 
 @Composable
-fun SeriesScreen(
+fun ShowsScreen(
     onClickShow: (Int) -> Unit,
     modifier: Modifier = Modifier,
     showViewModel: HomeViewModel = hiltViewModel(),
@@ -67,6 +70,7 @@ fun SeriesScreen(
                 scrollState = scrollState,
                 state = state,
                 onClickShow = onClickShow,
+                modifier = modifier
             )
         }
     }
@@ -77,13 +81,21 @@ private fun SuccessStateShowContent(
     scrollState: LazyGridState,
     state: ShowUiState.Success,
     onClickShow: (Int) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
+    val haze = remember {
+        HazeState()
+    }
     Column {
         CustomTopBarComponent(
             title = "All shows",
             modifier = Modifier.padding(horizontal = AppTheme.size.dp16)
         )
         LazyVerticalGrid(
+            modifier = Modifier.hazeChild(
+                state = haze,
+                style = HazeDefaults.style(backgroundColor = AppTheme.colorScheme.onBackground),
+            ),
             state = scrollState,
             columns = GridCells.Fixed(2),
             contentPadding = PaddingValues(horizontal = AppTheme.size.dp16),
