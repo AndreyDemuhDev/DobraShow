625f5e7792a2bf7f8b268e4a7630029363478109
Andrey Agnesh <AndreyAgnesh@yandex.by>
2024-10-18 23:21:04 +0300

    Setting new bottom bar navigation

--- app/src/main/java/com/example/dobrashow/navigation/DobraShowApp.kt
+++ app/src/main/java/com/example/dobrashow/navigation/DobraShowApp.kt
@@ -1,21 +1,23 @@
 package com.example.dobrashow.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.design.theme.AppTheme
 
 @Composable
-fun DobraShowApp(
-    modifier: Modifier = Modifier,
-    innerPadding: PaddingValues,
-) {
-
+fun DobraShowApp() {
     val navController = rememberNavController()
-
-    DobraShowHost(
-        navController = navController,
-        innerPadding = innerPadding,
-        modifier = modifier
-    )
+    Scaffold(
+        containerColor = AppTheme.colorScheme.background,
+        bottomBar = { BottomNavigationTabs(navController = navController) }
+    ) { innerPadding ->
+        DobraShowHost(
+            navController = navController,
+            innerPadding = innerPadding,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
